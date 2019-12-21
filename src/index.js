3f62208f49bd280b6fbca83f25dc74433cd0dbb2
ifjso <ojsboy@hotmail.com>
2019-12-21 15:03:38 +0900

    index.html 추가

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+// https://github.com/techhtml/2019-12-21-pwa-workshop
 const App = () => <div className="app">Hello Azure world</div>;
 
 ReactDOM.render(<App />, document.getElementById('app'));
