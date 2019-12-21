daeb506b43959986dddc186229b3a1bc0d45250e
ifjso <ojsboy@hotmail.com>
2019-12-21 15:42:30 +0900

    manifest 설정

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const WebpackPwaManifest = require('webpack-pwa-manifest');
 
 module.exports = {
   entry: './src/index.js',
@@ -21,6 +22,21 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: './src/index.html'
+    }),
+    new WebpackPwaManifest({
+      name: '리액트 메모앱',
+      short_name: '메모',
+      description: '리액트로 제작한 메모앱입니다.',
+      background_color: '#ffffff',
+      crossorigin: 'use-credentials',
+      theme_color: '#eeeeee',
+      icons: [
+        {
+          src: path.resolve('src/assets/Icon.png'),
+          sizes: [96, 128, 192, 256, 384, 512]
+        }
+      ]
     })
+    // https://github.com/techhtml/2019-12-21-pwa-workshop/blob/02_manifest/src/assets/Icon.png
   ]
 };
